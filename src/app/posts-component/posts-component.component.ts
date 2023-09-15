96898b81e90cf0f5fdfad0fd64450ba14b89de33
CharleneTSJ <charlenetansijie@outlook.com>
2023-09-16 01:31:02 +0800

    http delete operation

--- src/app/posts-component/posts-component.component.ts
+++ src/app/posts-component/posts-component.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 
 interface MyData{
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+  userId?: number;
+  id?: number;
+  title?: string;
+  body?: string;
 }
+
+
 @Component({
   selector: 'app-posts-component',
   templateUrl: './posts-component.component.html',
@@ -14,7 +16,7 @@ interface MyData{
 })
 
 export class PostsComponentComponent {
-  post:any;
+  post!:MyData[];
 constructor(private http:HttpClient){
   
   http.get<MyData[]>('https://jsonplaceholder.typicode.com/posts')
@@ -37,21 +39,25 @@ inputEntered(newPost:HTMLInputElement){
   )
 }
 
-updateClick(thisPost:HTMLElement,postID:number){
+updateClick(thisPost:MyData,postID:number|undefined,idx:number){
+  console.log("postID:",postID)
+  console.log("idx:",idx)
   console.log("update clicked")
+  console.log("thisPost argument:",typeof thisPost)
+  console.log("postID argument:", typeof postID)
   
- this.http.patch('https://jsonplaceholder.typicode.com/posts/${postID}',
- {"body":"charlene successfully do a patch"}).subscribe(
-  response=>{
-    console.log("Response printCharlene:"+response)
-    console.log("Response HE printCharlene:"+JSON.stringify(response))
-   
-    console.log("response end")
-    // console.log(response)
+ this.http.delete('https://jsonplaceholder.typicode.com/posts/${postID}').subscribe(
+  {next:response=>{
+    console.log("this post to delete: ",thisPost)
+    const indexToRemove = this.post.findIndex(item =>
+      item.id === thisPost.id 
+    );
+    console.log("indexToRemove:",indexToRemove)
+    this.post.splice(indexToRemove,1)
   },
-  (error)=>{
-    console.error("Update w failred: ",error)
-  }
+  error: (error)=>{
+    console.error("Update  failred: ",error)
+  }}
  )
 }
 
